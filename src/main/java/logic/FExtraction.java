168c4fa298aca758699c3365c9f903cfe49bdfae
Jirayus Jiarpakdee <jiyoyo@hotmail.com>
2015-10-23 22:03:09 +0900

    Tune the number of thread 4

--- src/main/java/logic/FExtraction.java
+++ src/main/java/logic/FExtraction.java
@@ -113,8 +113,8 @@ public class FExtraction {
 			+ " and id < " + ((index + 1) * querySize));
 	  try (Connection dbConnection = connectToDB();
 			ResultSet rs = executeQuery(dbConnection,
-				  "select * from question where id >=" + (index * querySize)
-						+ " and id < " + ((index + 1) * querySize));
+				  "select * from question limit " + querySize + " offset "
+						+ (index * querySize));
 			PreparedStatement preparedStatement = dbConnection
 				  .prepareStatement("INSERT INTO question_features values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")) {
 
