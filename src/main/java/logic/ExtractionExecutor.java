82cd0e0423211c019c81c73269fd053609a1d956
Jirayus Jiarpakdee <jiyoyo@hotmail.com>
2015-10-27 14:35:01 +0900

    Fix null character in text field

--- src/main/java/logic/ExtractionExecutor.java
+++ src/main/java/logic/ExtractionExecutor.java
@@ -30,7 +30,7 @@ public class ExtractionExecutor implements Callable {
 
    protected String getTextWithoutCode(Document inputHtmlDocument) {
 	  return (recursiveExtraction(inputHtmlDocument.head()) + recursiveExtraction(inputHtmlDocument
-			.body())).replace("\n", " ").trim();
+			.body())).replace("\n", " ").trim().replace("0x00", "");
    }
 
    protected String recursiveExtraction(Element inputElement) {
